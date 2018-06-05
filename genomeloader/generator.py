662ace1c8788fea6cab00ec004392984031dfd9c
daquang <daquang@umich.edu>
2018-06-05 17:14:35 -0400

    Another commit

--- genomeloader/generator.py
+++ genomeloader/generator.py
@@ -1,55 +1,117 @@
 import numpy as np
+import pandas as pd
+import pybedtools as pbt
+from pybedtools.bedtool import BEDToolsError
 import keras
+import sklearn.utils
+from .wrapper import BedWrapper, BedGraphWrapper
 
 
 class BedGenerator(keras.utils.Sequence):
-    def __init__(self, bed, genome, batch_size=128, seq_len=1000,  shuffle=True):
+    def __init__(self, bed, genome, bigwigs=[], batch_size=128, epochs=10, window_len=200, seq_len=1024,
+                 negatives_ratio=1, jitter_mode='sliding', shuffle=True, seed=0):
         # Initialization
         self.bed = bed
         self.genome = genome
+        self.bigwigs = bigwigs
         self.batch_size = batch_size
+        self.epochs = epochs
+        self.epoch_i = -1
+        self.labels_epoch_i = None
+        self.intervals_df_epoch_i = None
+        self.window_len = window_len
         self.seq_len = seq_len
+        self.negatives_ratio = negatives_ratio
+        self.jitter_mode = jitter_mode
         self.shuffle = shuffle
+        self.bed.bt.set_chromsizes(self.genome.chroms_size_pybedtools())
+        if self.negatives_ratio > 1:
+            self.negative_windows_epoch_i = BedWrapper(pbt.BedTool.cat(*(self.negatives_ratio * [self.bed.bt]), postmerge=False).saveas().fn)
+            self.negative_windows_epoch_i.bt.set_chromsizes(self.genome.chroms_size_pybedtools())
+        else:
+            self.negative_windows_epoch_i = self.bed
+        if self.jitter_mode == 'sliding':
+            self.cumulative_excl_bt = self.bed.bt.slop(b=self.window_len/2)
+        else:
+            self.cumulative_excl_bt = self.bed.bt
         self.on_epoch_end()
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.ceil(1.0 * len(self.bedgraph) / self.batch_size))
+        return int(np.ceil((1.0 + self.negatives_ratio) * len(self.bed) / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch of data'
         # Collect genome intervals of the batch
-        intervals_df = self.bedgraph[index*self.batch_size:(index+1)*self.batch_size]
-        X = []
+        intervals_batch_df = self.intervals_df_epoch_i[index*self.batch_size:(index+1)*self.batch_size]
+        labels_batch = self.labels_epoch_i[index*self.batch_size:(index+1)*self.batch_size]
+        x_genome = []
         y = []
-        for interval in intervals_df.itertuples():
+        for interval, label in zip(intervals_batch_df.itertuples(), labels_batch):
             chrom = interval[1]
             chromStart = interval[2]
             chromEnd = interval[3]
-            label = interval[4]
-            X.append(self.genome[chrom, chromStart:chromEnd])
+            midpt = int((chromStart + chromEnd) / 2)
+            start = midpt - self.seq_len / 2
+            stop = midpt + self.seq_len / 2
+            if self.jitter_mode == 'sliding':
+                shift_size = np.max((self.window_len / 2 + 1, chromEnd - self.window_len / 2 - chromStart + 1))
+            elif self.jitter_mode == 'landmark':
+                shift_size = midpt - chromStart
+            else:
+                shift_size = 0
+            s = np.random.randint(-shift_size, shift_size+1)
+            start += s
+            stop += s
+            x_genome.append(self.genome[chrom, start:stop])
             y.append(label)
 
-        X = np.array(X)
+        x_genome = np.array(x_genome)
+        if len(self.bigwigs) == 0:
+            x = x_genome
         y = np.array(y)
-        return X, y
+        return x, y
 
     def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        if self.shuffle:
-            self.bedgraph.shuffle()
-
-    def _generate_negatives(self):
-        return
+        self.epoch_i += 1
+        if self.epoch_i > 0 and not self.shuffle:
+            return
+        'Updates indexes after each epoch if shuffling is desired'
+        try:
+            self.cumulative_excl_bt = self.cumulative_excl_bt.cat(self.negative_windows_epoch_i.bt)
+            negative_windows_bt = self.negative_windows_epoch_i.bt.shuffle(excl=self.cumulative_excl_bt.fn, noOverlapping=True)
+            self.negative_windows_epoch_i = BedWrapper(negative_windows_bt.fn)
+            self.negative_windows_epoch_i.bt.set_chromsizes(self.genome.chroms_size_pybedtools())
+        except BEDToolsError:  # Cannot find any more non-overlapping intervals, reset
+            if self.negatives_ratio > 1:
+                self.negative_windows_epoch_i = BedWrapper(
+                    pbt.BedTool.cat(*(self.negatives_ratio * self.epochs * [self.bed.bt]), postmerge=False).saveas().fn)
+                self.negative_windows_epoch_i.bt.set_chromsizes(self.genome.chroms_size_pybedtools())
+            else:
+                self.negative_windows_epoch_i = self.bed
+            if self.jitter_mode == 'sliding':
+                self.cumulative_excl_bt = self.bed.bt.slop(b=self.window_len / 2)
+            else:
+                self.cumulative_excl_bt = self.bed.bt
+        labels_epoch_i = np.zeros((self.negatives_ratio + 1) * len(self.bed), dtype=bool)
+        labels_epoch_i[:len(self.bed)] = True
+        self.intervals_df_epoch_i = pd.concat([self.bed.df, self.negative_windows_epoch_i.df])
+        self.intervals_df_epoch_i, self.labels_epoch_i = sklearn.utils.shuffle(self.intervals_df_epoch_i,
+                                                                               labels_epoch_i)
 
 
 class BedGraphGenerator(keras.utils.Sequence):
-    def __init__(self, bedgraph, genome, batch_size=128, seq_len=1000,  shuffle=True):
+    def __init__(self, bedgraph, genome, batch_size=128, seq_len=1024, incl_chroms=None, shuffle=True):
         # Initialization
         self.bedgraph = bedgraph
         self.genome = genome
         self.batch_size = batch_size
         self.seq_len = seq_len
+        # Remove intervals that are not in included chromosomes (default: use all chromosomes)
+        self.incl_chroms = incl_chroms
+        if incl_chroms is not None:
+            new_bt = self.bedgraph.filter(lambda feature: feature.chrom in incl_chroms).saveas()
+            self.bedgraph = BedGraphWrapper(new_bt.fn)
         self.shuffle = shuffle
         self.on_epoch_end()
 
@@ -68,7 +130,10 @@ class BedGraphGenerator(keras.utils.Sequence):
             chromStart = interval[2]
             chromEnd = interval[3]
             label = interval[4]
-            X.append(self.genome[chrom, chromStart:chromEnd])
+            midpt = int((chromStart + chromEnd) / 2)
+            start = midpt - self.seq_len / 2
+            stop = midpt + self.seq_len / 2
+            X.append(self.genome[chrom, start:stop])
             y.append(label)
 
         X = np.array(X)
--- genomeloader/wrapper.py
+++ genomeloader/wrapper.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import pybedtools as pbt
+from collections import OrderedDict
 from pyfaidx import Fasta
 import py2bit
 import pyBigWig
@@ -7,25 +9,92 @@ from tqdm import tqdm
 import sklearn.utils
 
 
-class TwoBitWrapper:
-    def __init__(self, twobit_file, alpha='dna', one_hot=True, channel_last=True, in_mem=False, thread_safe=False):
-        self.twobit = py2bit.open(twobit_file)
+class SignalWrapper:
+    def __init__(self, in_mem=False, thread_safe=False):
+        self.in_mem = in_mem
+        self.thread_safe = thread_safe
+
+    def __del__(self):
+        self.close()
+
+    def __getitem__(self, item):
+        return
+
+    def close(self):
+        return
+
+    def chroms(self):
+        return self._chroms
+
+    def chroms_size(self):
+        return self._chroms_size
+
+    def chroms_size_pybedtools(self):
+        chromsizes = OrderedDict(self._chroms_size)
+        for chrom in chromsizes:
+            chromsizes[chrom] = (0, chromsizes[chrom])
+        return chromsizes
+
+    def __getitem__(self, item):
+        if len(item) == 2:
+            chrom, coords = item
+        else:
+            chrom = item
+            coords = slice(0, self._chroms_size[chrom], None)
+        # In practice, never have to worry if coords.start > chrom_size or coords.stop < 0
+        chrom_size = self._chroms_size[chrom]
+        start = int(max(0, coords.start))
+        stop = int(min(coords.stop, chrom_size))
+        seq = self._get_seq(chrom, start, stop)
+        seq_len = stop - start
+        orig_len = coords.stop - coords.start
+        if seq_len < orig_len:
+            left_seq = np.array((start - coords.start) * [self.default_value], dtype=seq.dtype)
+            right_seq = np.array((coords.stop - stop) * [self.default_value], dtype=seq.dtype)
+            seq = np.concatenate([left_seq, seq, right_seq])
+        return seq
+
+    def _get_seq(self, chrom, start, stop):
+        return None
+
+
+class GenomeWrapper(SignalWrapper):
+    def __init__(self, alpha='dna', one_hot=True, channel_last=True, in_mem=False, thread_safe=False):
+        super().__init__(in_mem, thread_safe)
+        alphabets = {
+            'dna': np.array(['A', 'C', 'G', 'T']),
+            'rna': np.array(['A', 'C', 'G', 'U']),
+            'protein': np.array(['A', 'C', 'D', 'E',
+                                 'F', 'G', 'H', 'I',
+                                 'K', 'L', 'M', 'N',
+                                 'P', 'Q', 'R', 'S',
+                                 'T', 'V', 'W', 'Y'])
+        }
+        ambiguous_letters = {
+            'dna': 'N',
+            'rna': 'N',
+            'protein': 'X'
+        }
+        self.channel_last = channel_last
+        self.residues = alphabets[alpha]
+        self.default_value = ambiguous_letters[alpha]
         self.one_hot = one_hot
+
+    def __getitem__(self, item):
+        seq = super().__getitem__(item)
+        if self.one_hot:
+            seq = self.residues == seq[:, np.newaxis]
+            if not self.channel_last:
+                seq = seq.T
+        return seq
+
+
+class TwoBitWrapper(GenomeWrapper):
+    def __init__(self, twobit_file, alpha='dna', one_hot=True, channel_last=True, in_mem=False, thread_safe=False):
+        super().__init__(alpha, one_hot, channel_last, in_mem, thread_safe)
+        self.twobit = py2bit.open(twobit_file)
         self._chroms = list(self.twobit.chroms().keys())
         self._chroms_size = self.twobit.chroms()
-        self.channel_last = channel_last
-        self.thread_safe = thread_safe
-        if alpha == 'dna':
-            self.residues = np.array(['A', 'C', 'G', 'T'])
-        elif alpha == 'rna':
-            self.residues = np.array(['A', 'C', 'G', 'U'])
-        elif alpha == 'protein':
-            self.residues = np.array(['A', 'C', 'D', 'E',
-                                      'F', 'G', 'H', 'I',
-                                      'K', 'L', 'M', 'N',
-                                      'P', 'Q', 'R', 'S',
-                                      'T', 'V', 'W', 'Y'])
-        self.in_mem = in_mem
         if in_mem:
             twobit_onehot_dict = self._encode_seqs(self.twobit)
             self.twobit.close()
@@ -36,14 +105,12 @@ class TwoBitWrapper:
                 self.twobit.close()
                 self.twobit = twobit_file
 
-    def __del__(self):
-        self.close()
-
     def close(self):
         if not self.thread_safe:
             self.twobit.close()
 
-    def _encode_seqs(self, twobit):
+    @staticmethod
+    def _encode_seqs(twobit):
         # Converts a TwoBit object into a dictionary of one-hot coded boolean matrices
         twobit_dict = {}
         pbar = tqdm(twobit.chroms())
@@ -51,61 +118,31 @@ class TwoBitWrapper:
             pbar.set_description(desc='Loading sequence: ' + chrom)
             seq = twobit.sequence(chrom)
             seq = np.array(list(seq))
-            if self.one_hot:
-                seq = self.residues == seq[:, np.newaxis]
-                if not self.channel_last:
-                    seq = seq.T
             twobit_dict[chrom] = seq
         return twobit_dict
 
-    def __getitem__(self, item):
-        if len(item) == 2:
-            chrom, coords = item
-        else:
-            chrom = item
-            coords = slice(0, self._chroms_size[chrom], None)
+    def _get_seq(self, chrom, start, stop):
         if self.in_mem:
-            seq = self.twobit[chrom][coords]
+            seq = self.twobit[chrom][start:stop]
         else:
             if self.thread_safe:
                 twobit = py2bit.open(self.twobit)
-                seq = np.array(list(twobit.sequence(chrom, coords.start, coords.stop)))
+                seq = np.array(list(twobit.sequence(chrom, start, stop)))
                 twobit.close()
             else:
-                seq = np.array(list(self.twobit.sequence(chrom, coords.start, coords.stop)))
-            if self.one_hot:
-                seq = self.residues == seq[:, np.newaxis]
-                if not self.channel_last:
-                    seq = seq.T
+                seq = np.array(list(self.twobit.sequence(chrom, start, stop)))
         return seq
 
-    def chroms(self):
-        return self._chroms
 
-    def chroms_size(self):
-        return self._chroms_size
-
-
-class FastaWrapper:
-    def __init__(self, fasta_file, alpha='dna', read_ahead=10000, one_hot=True, in_mem=False, thread_safe=False):
+class FastaWrapper(GenomeWrapper):
+    def __init__(self, fasta_file, alpha='dna', one_hot=True, channel_last=True, in_mem=False, thread_safe=False,
+                 read_ahead=10000):
+        super().__init__(alpha, one_hot, channel_last, in_mem, thread_safe)
         self.fasta = Fasta(fasta_file, as_raw=True, sequence_always_upper=True, read_ahead=read_ahead)
-        self.one_hot = one_hot
         self._chroms = list(self.fasta.keys())
         seq_lens = [len(self.fasta[chrom]) for chrom in self._chroms]
         self._chroms_size = dict(zip(self._chroms, seq_lens))
         self.read_ahead = read_ahead
-        self.thread_safe = thread_safe
-        if alpha == 'dna':
-            self.residues = np.array(['A', 'C', 'G', 'T'])
-        elif alpha == 'rna':
-            self.residues = np.array(['A', 'C', 'G', 'U'])
-        elif alpha == 'protein':
-            self.residues = np.array(['A', 'C', 'D', 'E',
-                                      'F', 'G', 'H', 'I',
-                                      'K', 'L', 'M', 'N',
-                                      'P', 'Q', 'R', 'S',
-                                      'T', 'V', 'W', 'Y'])
-        self.in_mem = in_mem
         if in_mem:
             fasta_onehot_dict = self._encode_seqs(self.fasta)
             self.fasta.close()
@@ -116,14 +153,12 @@ class FastaWrapper:
                 self.fasta.close()
                 self.fasta = fasta_file
 
-    def __del__(self):
-        self.close()
-
     def close(self):
         if not self.thread_safe:
             self.fasta.close()
 
-    def _encode_seqs(self, fasta):
+    @staticmethod
+    def _encode_seqs(fasta):
         # Converts a FASTA object into a dictionary of one-hot coded boolean matrices
         fasta_dict = {}
         pbar = tqdm(fasta)
@@ -131,45 +166,29 @@ class FastaWrapper:
             pbar.set_description(desc='Loading sequence: ' + record.name)
             seq = record[:]
             seq = np.array(list(seq))
-            if self.one_hot:
-                seq = self.residues == seq[:, np.newaxis]
             fasta_dict[record.name] = seq
         return fasta_dict
 
-    def __getitem__(self, item):
-        if len(item) == 2:
-            chrom, coords = item
-        else:
-            chrom = item
-            coords = slice(0, self._chroms_size[chrom], None)
+    def _get_seq(self, chrom, start, stop):
         if self.in_mem:
-            seq = self.fasta[chrom][coords]
+            seq = self.fasta[chrom][start:stop]
         else:
             if self.thread_safe:
                 fasta = Fasta(self.fasta, as_raw=True, sequence_always_upper=True, read_ahead=self.read_ahead)
-                seq = np.array(list(fasta[chrom][coords]))
+                seq = np.array(list(fasta[chrom][start:stop]))
                 fasta.close()
             else:
-                seq = np.array(list(self.fasta[chrom][coords]))
-            if self.one_hot:
-                seq = self.residues == seq[:, np.newaxis]
+                seq = np.array(list(self.fasta[chrom][start:stop]))
         return seq
 
-    def chroms(self):
-        return self._chroms
 
-    def chroms_size(self):
-        return self._chroms_size
-
-
-class BigWigWrapper:
-    def __init__(self, bigwig_file, default_value=0, in_mem=False, thread_safe=False):
+class BigWigWrapper(SignalWrapper):
+    def __init__(self, bigwig_file, in_mem=False, thread_safe=False, default_value=0):
+        super().__init__(in_mem, thread_safe)
         self.bigwig = pyBigWig.open(bigwig_file, 'r')
         self._chroms_size = self.bigwig.chroms()
-        self._chroms = list(self._chroms_size.chroms())
+        self._chroms = list(self._chroms_size.keys())
         self.default_value = default_value
-        self.thread_safe = thread_safe
-        self.in_mem = in_mem
         if in_mem:
             seqs_dict = self._encode(self.bigwig)
             self.bigwig.close()
@@ -179,9 +198,6 @@ class BigWigWrapper:
             if thread_safe:
                 self.bigwig.close()
                 self.bigwig = bigwig_file
-
-    def __del__(self):
-        self.close()
 
     def close(self):
         if not self.thread_safe:
@@ -196,32 +212,42 @@ class BigWigWrapper:
             seqs_dict[chrom] = self.bigwig.values(chrom, 0, self._chroms_size[chrom], numpy=True)
         return seqs_dict
 
-    def __getitem__(self, item):
-        if len(item) == 2:
-            chrom, coords = item
-        else:
-            chrom = item
-            coords = slice(0, self._chroms_size[chrom], None)
+    def _get_seq(self, chrom, start, stop):
         if self.in_mem:
-            seq = self.bigwig[chrom][coords]
+            seq = self.bigwig[chrom][start:stop]
         else:
             if self.thread_safe:
                 bigwig = pyBigWig.open(self.bigwig, 'r')
-                seq = bigwig.values(chrom, coords.start, coords.stop, numpy=True)
+                seq = bigwig.values(chrom, start, stop, numpy=True)
                 bigwig.close()
             else:
-                seq = self.bigwig.values(chrom, coords.start, coords.stop, numpy=True)
+                seq = self.bigwig.values(chrom, start, stop, numpy=True)
         seq[np.isnan(seq)] = self.default_value
         return seq
 
-    def chroms(self):
-        return self._chroms
+
+class BedWrapper:
+    def __init__(self, bed_file):
+        self.df = pd.read_table(bed_file,
+                                names=['chrom', 'chromStart', 'chromEnd'],
+                                usecols=[0, 1, 2])
+        self.bt = pbt.BedTool(bed_file).sort()
+
+    def __getitem__(self, item):
+        return self.df.iloc[item]
+
+    def __len__(self):
+        return len(self.df)
+
+    def shuffle(self):
+        self.df = sklearn.utils.shuffle(self.df)
 
 
 class BedGraphWrapper:
     def __init__(self, bedgraph_file):
         self.df = pd.read_table(bedgraph_file,
                            names=['chrom', 'chromStart', 'chromEnd', 'dataValue'])
+        self.bt = pbt.BedTool(bedgraph_file).sort()
 
     def __getitem__(self, item):
         return self.df.iloc[item]
@@ -236,7 +262,9 @@ class BedGraphWrapper:
 class NarrowPeakWrapper:
     def __init__(self, narrowpeak_file):
         self.df = pd.read_table(narrowpeak_file,
-                           names=['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand', 'signalValue', 'pValue', 'qValue', 'peak'])
+                           names=['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand',
+                                  'signalValue', 'pValue', 'qValue', 'peak'])
+        self.bt = pbt.BedTool(narrowpeak_file).sort()
 
     def __getitem__(self, item):
         return self.df.iloc[item]
