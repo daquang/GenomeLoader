6cfca6a07f951b4f3d7768c98280431caf4ec9f6
daquang <daniel.quang@gmail.com>
2019-09-27 23:42:01 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(version='0.1.0',
       name='genomeloader',
-      description="fast data loading for deep learning genomics",
+      description='fast data loading for deep learning genomics',
       long_description=open('README.md').read(),
       url='https://github.com/daquang/GenomeLoader',
-      license = "MIT",
-      author="Daniel Quang",
-      author_email="daquang@umich.edu",
+      license='MIT',
+      author='Daniel Quang',
+      author_email='daquang@umich.edu',
       packages=find_packages(),
       install_requires=['numpy', 'pybedtools', 'pyfaidx', 'pandas', 'keras',
                         'pyBigWig', 'py2bit', 'tqdm', 'scikit-learn', 'intervaltree']
